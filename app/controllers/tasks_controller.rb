03648b331b4db0330fd8f0a3b36a7442489b5dd0
aozametatuki <akomatsu1224@gmail.com>
2021-03-21 23:17:23 +0900

    登録機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,10 +5,29 @@ class TasksController < ApplicationController
   def show
   end
 
+  # 新規登録画面
   def new
+    # オブジェクト生成
     @task = Task.new
   end
 
+  # 登録
+  def create
+    # オブジェクト生成
+    task = Task.new(task_params)
+    # DBに保存
+    task.save!
+    # 一覧画面にリダイレクト
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
   def edit
+  end
+
+  private
+  
+  # 不正データでないことをチェック
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
