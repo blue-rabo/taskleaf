141c65f9ef2647d23605668bff7a223850f5d3d2
aozametatuki <akomatsu1224@gmail.com>
2021-04-12 23:03:01 +0900

    DBにnotnull制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_031516) do
+ActiveRecord::Schema.define(version: 2021_04_12_135410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
