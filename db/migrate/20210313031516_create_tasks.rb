5e95fd2fea8efda90427df59cf2caaaaf9df41b8
aozametatuki <akomatsu1224@gmail.com>
2021-04-14 21:50:15 +0900

    DBのカラムの文字列の長さを指定

--- db/migrate/20210313031516_create_tasks.rb
+++ db/migrate/20210313031516_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :name, null: false
+      t.string :name, limit: 30, null: false
       t.text :description
 
       t.timestamps
